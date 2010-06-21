091d967ef662733d6b126c90dcb6ab133e2d2740
Asgeir Frimannsson <asgeirf@gmail.com>
2010-06-21 23:29:47 +1000

    add files missed in previous changeset

--- src/main/java/com/unsercorp/ubergloss/client/UberGloss.java
+++ src/main/java/com/unsercorp/ubergloss/client/UberGloss.java
@@ -1,13 +1,29 @@
 package com.unsercorp.ubergloss.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.unsercorp.ubergloss.shared.rpc.MyService;
+import com.unsercorp.ubergloss.shared.rpc.MyServiceAsync;
 
 public class UberGloss implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		Window.alert("All good!");
+		MyServiceAsync myService = (MyServiceAsync) GWT.create(MyService.class);
+		
+		myService.sayHello( new AsyncCallback<String>() {
+			@Override
+			public void onSuccess(String result) {
+				Window.alert(result);
+			}
+			
+			@Override
+			public void onFailure(Throwable caught) {
+				Window.alert(caught.getMessage());
+			}
+		});
 	}
 
 }
